9a8d0e753b889a2b959dc38e7a936fc068c0fc4f
Sravani-Solo <sravani.kotyada@outlook.com>
2024-09-01 16:49:46 +0530

    added script

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-# ID=$(id -u)
-echo "Hello World"
+ID=$(id -u)
+R="/e[31m"
+G="/e[32m"
+Y="/e[33m"
+N="/e[0m"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$1... is $R failed $N"
+    else
+        echo -e "$1... is $G Success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]; then
+    echo "You are not a root user, try using sudo access"
+    exit 1
+fi
+
+echo -e "Script excecution $Y started $N"
+VALIDATE $?
+
