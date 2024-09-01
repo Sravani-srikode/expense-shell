62a6fece55e6212b5179b7cf9b8a0fde34df6ae2
Sravani-Solo <sravani.kotyada@outlook.com>
2024-09-01 19:01:17 +0530

    Started MySQL

--- mysql.sh
+++ mysql.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE=$($LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log)
+mkdir -p $LOGS_FOLDER
+
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "$2... is $R failed $N"
+        echo -e "$2... is $R failed $N" | tee -a $LOG_FILE
     else
-        echo -e "$2... is $G Success $N"
+        echo -e "$2... is $G Success $N" | tee -a $LOG_FILE
     fi
 }
+
+echo "Srript started at: $(date)" | tee -a $LOG_FILE
 
 if [ $ID -ne 0 ]; then
     echo -e "$R You are not a root user $N, $Y try using sudo access $N"
@@ -21,24 +28,24 @@ if [ $ID -ne 0 ]; then
 fi
 
 # Developer has chosen the database MySQL. Install MySQL Server 8.0.x
-dnf install mysql-server -y
+dnf install mysql-server -y &>>LOG_FILE
 VALIDATE $? "Installing mySQL server"
 
 # Start MySQL Service
 systemctl enable mysqld
-VALIDATE $? "Enabling MySQL"
+VALIDATE $? "Enabling MySQL" &>>LOG_FILE
 
 systemctl start mysqld
-VALIDATE $? "Started MySQL"
+VALIDATE $? "Starting MySQL" &>>LOG_FILE
 
 # change the default root password in order to start using the database service. 
 # Using password ExpenseApp@1
 
 if [ $? -ne 0 ]; then
-    echo -e "MySQL Password not set up, $Y SETTING NOW $N"
+    echo -e "MySQL Password not set up, $Y SETTING NOW $N"  | tee -a $LOG_FILE
     mysql_secure_installation --set-root-pass ExpenseApp@1
     VALIDATE $? "Setting password"
 else
-    echo -e "MySQL password already set up.. $Y SKIPPING $N"
+    echo -e "MySQL password already set up.. $Y SKIPPING $N" | tee -a $LOG_FILE
 
 fi
